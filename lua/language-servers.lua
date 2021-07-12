4bfae68e3624d0aa3a2a4966956d71d33cc888cd
Thomas Steven <thomaspatricksteven@gmail.com>
2021-07-12 00:06:32 -0400

    formatters

--- lua/language-servers.lua
+++ lua/language-servers.lua
@@ -198,12 +198,19 @@ nvim_lsp.tsserver.setup {
     end
 }
 
-
-require "format".setup {
+require"format".setup {
     ["*"] = {
         {cmd = {"sed -i 's/[ \t]*$//'"}} -- remove trailing whitespace
     },
-    javascript = {
+    css = {{cmd = {"prettier -w"}}},
+    html = {{cmd = {"prettier -w"}}},
+    lua = {{cmd = {"lua-format -i"}}},
+    javascript = {{cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}},
+    typescript = {{cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}},
+    javascriptreact = {
         {cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}
     },
+    typescriptreact = {
+        {cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}
+    }
 }
